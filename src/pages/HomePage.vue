ecb807a059bbf6842a30167fc924178bedb87a73
Kashif Sulehria <captainkashi4@gmail.com>
2023-06-01 19:16:02 +0500

    login page link

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -2,6 +2,8 @@
   <div>
     <h1>Home Page</h1>
     <router-link to="/example" class="underline">Go to Example Page</router-link>
+    <br><br>
+    <router-link to="/login" class="underline">Go to Login Page</router-link>
   </div>
 </template>
 
