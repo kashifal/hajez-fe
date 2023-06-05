40e46e5e02e86f307e62f7c2ddde9abe80118596
Kashif Sulehria <captainkashi4@gmail.com>
2023-06-05 16:06:39 +0500

    home page added

---
+++ src/components/HomePageComponents/DropDown.vue
@@ -0,0 +1,50 @@
+<template>
+  <Listbox as="div" v-model="selected">
+    <ListboxLabel class="sr-only">Change published status</ListboxLabel>
+    <div class="relative">
+      <div class="inline-flex divide-x divide-primaryGreen rounded-md shadow-sm">
+        <div class="inline-flex items-center gap-x-1.5 rounded-l-md bg-primaryGreen px-3 py-2 text-white shadow-sm">
+          <CheckIcon class="-ml-0.5 h-5 w-5" aria-hidden="true" />
+          <p class="text-sm font-semibold">{{ selected.description }}</p>
+        </div>
+        <ListboxButton class="inline-flex items-center rounded-l-none rounded-r-md bg-primaryGreen p-2 hover:bg-primaryGreen focus:outline-none focus:ring-2 focus:ring-primaryGreen focus:ring-offset-2 focus:ring-offset-gray-50">
+          <span class="sr-only">Change published status</span>
+          <ChevronDownIcon class="h-5 w-5 text-white" aria-hidden="true" />
+        </ListboxButton>
+      </div>
+
+      <transition leave-active-class="transition ease-in duration-100" leave-from-class="opacity-100" leave-to-class="opacity-0">
+        <ListboxOptions class="absolute right-0 z-10 mt-2 w-72 origin-top-right divide-y divide-gray-200 overflow-hidden rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <ListboxOption as="template" v-for="option in publishingOptions" :key="option.title" :value="option" v-slot="{ active, selected }">
+            <li :class="[active ? 'bg-primaryGreen text-white' : 'text-gray-900', 'cursor-default select-none p-4 text-sm']">
+              <div class="flex flex-col">
+                <div class="flex justify-between">
+                  <p :class="selected ? 'font-semibold' : 'font-normal'">{{ option.title }}</p>
+                  <span v-if="selected" :class="active ? 'text-white' : 'text-primaryGreen'">
+                    <CheckIcon class="h-5 w-5" aria-hidden="true" />
+                  </span>
+                </div>
+                <p :class="[active ? 'text-indigo-200' : 'text-gray-500', 'mt-2']">{{ option.description }}</p>
+              </div>
+            </li>
+          </ListboxOption>
+        </ListboxOptions>
+      </transition>
+    </div>
+  </Listbox>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { Listbox, ListboxButton, ListboxLabel, ListboxOption, ListboxOptions } from '@headlessui/vue'
+import { CheckIcon, ChevronDownIcon } from '@heroicons/vue/20/solid'
+
+const publishingOptions = [
+  { title: 'Booking', description: 'Last 7 Days', current: true },
+  { title: 'Booking', description: 'Last 14 Days', current: true },
+  { title: 'Booking', description: 'Last 30 Days', current: true },
+  { title: 'Booking', description: 'Last 2 Months', current: true }, 
+]
+
+const selected = ref(publishingOptions[0])
+</script>
